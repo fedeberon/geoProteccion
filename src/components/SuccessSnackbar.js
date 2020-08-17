26e0bcda0c4d144e4efc0c659b9a3a793c9f5380
fedeberon <federicoberon@outlook.com>
2020-08-17 11:12:47 -0300

    Fede *Se agrega funcion alerta material UI. Se cambia estilo de login

---
+++ src/components/SuccessSnackbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import Slide from '@material-ui/core/Slide';
+
+function TransitionLeft(props) {
+  return <Slide {...props} direction="left" />;
+}
+
+function TransitionUp(props) {
+  return <Slide {...props} direction="up" />;
+}
+
+function TransitionRight(props) {
+  return <Slide {...props} direction="right" />;
+}
+
+function TransitionDown(props) {
+  return <Slide {...props} direction="down" />;
+}
+
+export default function NotificationComponent(){
+  const [open, setOpen] = React.useState(true);
+  const [transition, setTransition] = React.useState(undefined);
+
+  const handleClick = (Transition) => () => {
+    setTransition(() => Transition);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Snackbar
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={TransitionDown}
+        message="I love snacks"
+        key={transition ? transition.name : ''}
+      />
+    </div>
+  );
+}
