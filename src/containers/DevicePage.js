9f9491d3b3ad9a22dc176be3900a891e0ba7fc92
reckif <facundorecki17@gmail.com>
2020-09-14 14:22:15 -0300

    tabla divicesPage

--- src/containers/DevicePage.js
+++ src/containers/DevicePage.js
@@ -29,20 +29,20 @@ const useStyles = makeStyles(theme => ({
   },
   DivicePageSize : {
     float: 'right',
-    width:'70%',
-    marginRight: '10%',
+    width:'75%',
+    marginRight: '5%',
     marginTop: '6%',
   }
 }));
 
 
-function createData(field, userData,) {
-  return { field, userData,  };
+function createData(Id, Name, Uniqueld, Status, Disable, lastUpdate, Positionid, Groupid, Phone, Contact, Category, Geofenceids, Attributes, ) {
+  return { Id, Name, Uniqueld, Status, Disable, lastUpdate, Positionid, Groupid, Phone, Contact, Category, Geofenceids, Attributes, };
 }
 
 
 const rows = [
-  createData("Id:", "", ),
+  createData("", "", ),
   createData("Name:", "", ),
   createData("Uniqueld:", "",),
   createData("Status:", "", ),
@@ -110,15 +110,39 @@ const DevicePage = () => {
             <Table className={classes.table} aria-label="simple table">
               <TableHead>
                 <TableRow>
+                  <TableCell>Id</TableCell>
+                  <TableCell align="right">Name</TableCell>
+                  <TableCell align="right">Uniqueld</TableCell>
+                  <TableCell align="right">Status</TableCell>
+                  <TableCell align="right">Disable</TableCell>
+                  <TableCell align="right">lastUpdate</TableCell>
+                  <TableCell align="right">Positionid</TableCell>
+                  <TableCell align="right">Groupid</TableCell>
+                  <TableCell align="right">Phone</TableCell>
+                  <TableCell align="right">Contact</TableCell>
+                  <TableCell align="right">Category</TableCell>
+                  <TableCell align="right">Geofenceids</TableCell>
+                  <TableCell align="right">Attributes</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {rows.map((row) => (
-                  <TableRow key={row.field}>
+                  <TableRow key={row.id}>
                     <TableCell component="th" scope="row">
                       {row.field}
                     </TableCell>
-                    <TableCell align="left">{row.userData}</TableCell>
+                    <TableCell align="left">{row.Name}</TableCell>
+                    <TableCell align="left">{row.Uniqueld}</TableCell>
+                    <TableCell align="left">{row.Status}</TableCell>
+                    <TableCell align="left">{row.Disable}</TableCell>
+                    <TableCell align="left">{row.lastUpdate}</TableCell>
+                    <TableCell align="left">{row.Positionid}</TableCell>
+                    <TableCell align="left">{row.Groupid}</TableCell>
+                    <TableCell align="left">{row.Phone}</TableCell>
+                    <TableCell align="left">{row.Contact}</TableCell>
+                    <TableCell align="left">{row.Category}</TableCell>
+                    <TableCell align="left">{row.Geofenceids}</TableCell>
+                    <TableCell align="left">{row.Attributes}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
