a7662fd7fb066729f25f786e9d52d882ae273d42
pabloIVI <pablo5472>
2020-11-06 17:59:07 -0300

    pi- Get & Post Notifications done.

--- src/containers/NotificationsPage.js
+++ src/containers/NotificationsPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import * as service from "../utils/serviceManager";
 import {makeStyles} from '@material-ui/core/styles';
@@ -18,16 +18,19 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import {DeleteTwoTone} from "@material-ui/icons";
+import {DeleteTwoTone, Label} from "@material-ui/icons";
 import EditTwoToneIcon from '@material-ui/icons/EditTwoTone';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
+import AddIcon from "@material-ui/icons/Add";
+import TextField from "@material-ui/core/TextField";
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import useTheme from "@material-ui/core/styles/useTheme";
+import InputLabel from "@material-ui/core/InputLabel";
+import Switch from '@material-ui/core/Switch';
 
 const useStyles = makeStyles((theme) => ({
 
@@ -35,13 +38,11 @@ const useStyles = makeStyles((theme) => ({
     overflowY: 'scroll',
     height: '100%',
     overflowX: 'hidden',
-    paddingBottom: '15%',
+    paddingBottom: '5%',
     [theme.breakpoints.up('md')]: {
       width: '100%',
       height: '100%',
       overflowY: 'scroll',
-      paddingTop: '5%',
-      paddingRight: '15%',
       flexGrow: 1,
       backgroundColor: theme.palette.background.paper,
     },
@@ -49,7 +50,37 @@ const useStyles = makeStyles((theme) => ({
   table: {
     minWidth: 650,
   },
+  formControlType: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  chipsTypes: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  chipTypes: {
+    margin: 2,
+  },
+  noLabelTypes: {
+    marginTop: theme.spacing(3),
+  },
 }));
+
+const styles = (theme) => ({
+  rootNotification: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
 
 function TabPanel(props) {
   const {children, value, index, ...other} = props;
@@ -90,29 +121,103 @@ export default function NotificationsPage() {
   const [value, setValue] = React.useState(0);
   const userId = useSelector((state) => state.session.user.id);
   const [notifications, setNotifications] = React.useState([])
-  const [openConfirm, setOpenConfirm] = React.useState(false);
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
-  const [confirm, setConfirm] = React.useState(false);
   const [idToRemove, setIdToRemove] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+  const [type, setType] = React.useState('');
+  const [always, setAlways] = useState(false);
+  const [notificators, setNotificators] = useState('')
+  const typesList = [
+    `${t('eventDeviceUnknown')}`,
+    `${t('eventDeviceOnline')}`,
+    `${t('eventTextMessage')}`,
+    `${t('eventDeviceFuelDrop')}`,
+    `${t('eventDeviceOverspeed')}`,
+    `${t('eventGeofenceEnter')}`,
+    `${t('eventGeofenceExit')}`,
+    `${t('eventCommandResult')}`,
+    `${t('eventMaintenance')}`,
+    `${t('eventDriverChanged')}`,
+    `${t('eventDeviceOffline')}`,
+    `${t('eventIgnitionOff')}`,
+    `${t('eventIgnitionOn')}`,
+    `${t('eventDeviceMoving')}`,
+    `${t('eventDeviceStopped')}`,
+    `${t('eventAlarm')}`,
+  ]
+
+  const typesValues = [
+    'DeviceUnknown',
+    'DeviceOnline',
+    'TextMessage',
+    'DeviceFuelDrop',
+    'DeviceOverspeed',
+    'GeofenceEnter',
+    'GeofenceExit',
+    'CommandResult',
+    'Maintenance',
+    'DriverChanged',
+    'DeviceOffline',
+    'IgnitionOff',
+    'IgnitionOn',
+    'DeviceMoving',
+    'DeviceStopped',
+    'Alarm',
+  ]
+
+
+  const handleAlwaysChecked = () => {
+    setAlways(!always);
+  }
+
+  const handleChangeType = (event) => {
+    setType(event.target.value);
+  };
+
+  const handleChangeChannel = (event) => {
+    setNotificators(event.target.value);
+  }
+
+  const getChannel = () => {
+    console.log(notificators);
+  }
+
+  const handleOpenNotification = () => {
+    setOpen(true);
+  };
+
+  const handleCloseNotification = () => {
+    setOpen(false);
+  };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const handleClickOpenConfirm = (param) => {
-    setOpenConfirm(true);
-    setIdToRemove(param)
-  };
-
-  const handleCloseConfirm = () => {
-    setOpenConfirm(false);
-  };
-
   const getNotifications = async (userId) => {
     const response = await service.getNotificationsByUserId(userId);
-      setNotifications(response);
-      getNotifications(userId);
+    setNotifications(response);
+    getNotifications(userId);
+  }
+
+  const postNotifications = () => {
+      const addNotification = {}
+        addNotification.type = type;
+        addNotification.always = always;
+        addNotification.notificators = notificators;
+
+        console.log(JSON.stringify(addNotification))
+
+        fetch(`api/notifications?userId=${userId}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+          body: JSON.stringify(addNotification)
+        }).then(response => console.log(response))
+    getNotifications(userId);
+    handleCloseNotification();
   }
 
   // const removeConfirm = () => {
@@ -134,13 +239,18 @@ export default function NotificationsPage() {
 
 
   return (
-    <div>
+    <div className={classes.root}>
       <div style={{marginTop: '5%'}} className="title-section">
         <h2>{t('sharedNotifications')}</h2>
         <Divider/>
       </div>
-      {/*<button type="button "onClick={getNotificationsByUserId}>Obtener notif</button>*/}
-      <div className={classes.root}>
+      <Button style={{margin: '10px 0px'}} type="button" color="primary"
+              variant="outlined"
+              onClick={handleOpenNotification}>
+        <AddIcon color="primary"/>
+        Crear nueva notificacion
+      </Button>
+
         <AppBar position="static" color="default">
           <Tabs
             value={value}
@@ -151,7 +261,8 @@ export default function NotificationsPage() {
             scrollButtons="auto"
             aria-label="scrollable auto tabs example"
           >
-            <Tab onClick={() => getNotifications(userId)} label="By User ID" {...a11yProps(0)} />
+            <Tab onClick={() => getNotifications(userId)}
+                 label="By User ID" {...a11yProps(0)} />
             <Tab label="By Device ID" {...a11yProps(1)} />
             <Tab label="By Group ID" {...a11yProps(2)} />
             <Tab label="All Notifications" {...a11yProps(3)} />
@@ -176,9 +287,11 @@ export default function NotificationsPage() {
                     <TableRow key={index}>
                       <TableCell align="center">{notification.id}</TableCell>
                       <TableCell align="center">{notification.type}</TableCell>
-                      <TableCell align="center">{`${Boolean(notification.always)}`}</TableCell>
+                      <TableCell
+                        align="center">{`${Boolean(notification.always)}`}</TableCell>
                       <TableCell align="center">Undefined</TableCell>
-                      <TableCell align="center">{notification.notificators}</TableCell>
+                      <TableCell
+                        align="center">{notification.notificators}</TableCell>
                       <TableCell align="center">
                         <EditTwoToneIcon/>
                         <DeleteTwoTone/>
@@ -209,28 +322,75 @@ export default function NotificationsPage() {
         <TabPanel value={value} index={6}>
           Item Seven
         </TabPanel>
-      </div>
 
-      {/*Dialog to confirm remove*/}
       <div>
-        <Dialog
-          fullScreen={fullScreen}
-          open={openConfirm}
-          onClose={handleCloseConfirm}
-          aria-labelledby="responsive-dialog-title"
-        >
-          <DialogTitle id="responsive-dialog-title">{"Use Google's location service?"}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              ¿Eliminar notificación?
-            </DialogContentText>
+        {/*Modal Add New Notification*/}
+        <Dialog onClose={handleCloseNotification}
+                aria-labelledby="customized-dialog-title"
+                open={open}>
+          <DialogTitle id="customized-dialog-title"
+                       onClose={handleCloseNotification}>
+            {` Agregar nueva Notificacion `}
+            <Button onClick={handleCloseNotification}>X</Button>
+          </DialogTitle>
+
+          <DialogContent dividers>
+            <form className={classes.rootNotification} noValidate
+                  autoComplete="off">
+              <Table>
+                <TableRow>
+                  <TableCell>Tipo:</TableCell>
+                  <TableCell>
+                    <FormControl variant="outlined" className={classes.formControlType}>
+                      <Select
+                        native
+                        value={type}
+                        onChange={handleChangeType}
+                      >
+                        <option aria-label="None" value="" />
+                        {typesValues.map((types, index) => (
+                          <option key={index} value={types}>{types}</option>
+                          ))}
+                      </Select>
+                    </FormControl>
+                  </TableCell>
+                </TableRow>
+              <TableRow>
+                <TableCell>Aplicar a todos los dispositivos:</TableCell>
+                <TableCell>
+                  <Switch
+                    onChange={handleAlwaysChecked}
+                    name="all"
+                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Canales:</TableCell>
+                <TableCell>
+                  <FormControl variant="outlined" className={classes.formControlType}>
+                    <Select
+                      native
+                      value={notificators}
+                      onChange={handleChangeChannel}
+                    >
+                      <option aria-label="None" value="" />
+                      <option value='web'>Web</option>
+                      <option value='correo'>Correo</option>
+                      <option value='sms'>Sms</option>
+                    </Select>
+                  </FormControl>
+                </TableCell>
+              </TableRow>
+              </Table>
+            </form>
           </DialogContent>
           <DialogActions>
-            <Button autoFocus onClick={handleCloseConfirm} color="primary">
-              Cancelar
+            <Button onClick={handleCloseNotification} autoFocus color="primary">
+              {t('sharedCancel')}
             </Button>
-            <Button color="primary" autoFocus>
-              Eliminar
+            <Button onClick={postNotifications} autoFocus color="primary">
+              {t('sharedSave')}
             </Button>
           </DialogActions>
         </Dialog>
