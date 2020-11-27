785844619484993b5fe288977ac905b7562f0428
pabloIVI <pablo5472>
2020-11-27 14:56:10 -0300

    pi- gradient colors changed

--- src/components/MainToolbar.js
+++ src/components/MainToolbar.js
@@ -33,7 +33,7 @@ const useStyles = makeStyles(theme => ({
   },
   titlePage: {
     backgroundColor: 'cadetblue',
-    backgroundImage: 'linear-gradient(25deg, #2dc6d4 0%, #1c2939 100%)',
+    backgroundImage: 'linear-gradient(25deg, #77F9D3 0%, #5CD2F8 50%, #5A79FF 100%);',
     boxShadow: 'rgba(102, 97, 102, 0.8) 0px 0px 15px 5px',
     display: 'inline-flex', justifyContent: 'space-between',
     position: 'absolute',
