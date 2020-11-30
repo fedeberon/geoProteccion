565edc1597621bc06b8911fdb7f57163c4d20970
LucasACortes <corteslucasa@outlook.com>
2020-11-30 11:24:56 -0300

    Popup position details

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,4 +1,5 @@
 const breakpoints = ['xs', 'sm', 'md', 'lg', 'xl'];
+const courses = ['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE'];
 
 const getBreakpointFromWidth = ( width ) => {
   return breakpoints[ ( width >= 1920 * 1 ) + ( width >= 1280 * 1 ) + ( width >= 960 * 1 ) + ( width >= 600 * 1 ) ];
@@ -40,8 +41,13 @@ const convertToCsv = (columns, data) => {
 	return csv;
 }
 
+const getCourse = (degrees) => {
+	return courses[ (degrees > 45 * 1) + (degrees > 90 * 1) + (degrees > 135 * 1) + (degrees > 180 * 1) + (degrees > 225 * 1) + (degrees > 270 * 1) + (degrees > 315 * 1)];
+}
+
 export {
   getBreakpointFromWidth,
   downloadCsv,
-  convertToCsv
+  convertToCsv,
+  getCourse,
 }
